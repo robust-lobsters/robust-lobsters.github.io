3c3dc0938ea8bfc60086dd3bd3dcabe97a77737d
vvyre <seung.yoon.yu@gmail.com>
2025-04-18 14:11:34 +0900

    하단 여백 추가

--- _lib/components/layout/grid.css.ts
+++ _lib/components/layout/grid.css.ts
@@ -3,7 +3,7 @@ import { style } from '@vanilla-extract/css'
 export const GRID = style({
   width: '100%',
   maxWidth: '45rem',
-  padding: '2rem 1rem 3rem 1rem',
+  padding: '2rem 1rem 10rem 1rem',
   height: '100%',
   minHeight: '100dvh',
   margin: '0 auto',
