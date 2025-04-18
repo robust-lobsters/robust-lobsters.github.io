835af17e0e68694b206a6b714bf2653b10633267
vvyre <seung.yoon.yu@gmail.com>
2025-04-18 12:19:50 +0900

    basepath 설정 롤백

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ const repo = 'https://robust-lobsters.github.io/robust-lobsters.github.io'
 
 const withVanillaExtract = createVanillaExtractPlugin()
 const nextConfig: NextConfig = {
-  basePath: process.env.NODE_ENV === 'production' ? `${repo}` : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? `${repo}/` : '',
   output: 'export',
   distDir: 'out',
