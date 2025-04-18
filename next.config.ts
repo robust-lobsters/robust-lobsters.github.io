c210a045318f8d6f2485cd78a194c5db2ffd4717
vvyre <seung.yoon.yu@gmail.com>
2025-04-18 12:17:25 +0900

    정적 자원 로드를 위한 path 설정 수정

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,13 @@
-import type { NextConfig } from 'next';
-import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin';
+import type { NextConfig } from 'next'
+import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin'
+const repo = 'https://robust-lobsters.github.io/robust-lobsters.github.io'
 
-const withVanillaExtract = createVanillaExtractPlugin();
+const withVanillaExtract = createVanillaExtractPlugin()
 const nextConfig: NextConfig = {
-  assetPrefix:
-    process.env.NODE_ENV === 'production' ? 'https://robust-lobsters.github.io/robust-lobsters.github.io' : '',
+  basePath: process.env.NODE_ENV === 'production' ? `/${repo}` : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? `/${repo}/` : '',
   output: 'export',
   distDir: 'out',
-};
+}
 
-export default withVanillaExtract(nextConfig);
+export default withVanillaExtract(nextConfig)
