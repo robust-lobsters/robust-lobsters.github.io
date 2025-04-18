e3a23b4741db5ecbcc4ded405ce7f27cd673eb7a
vvyre <seung.yoon.yu@gmail.com>
2025-04-18 12:17:43 +0900

    오타 수정

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,8 @@ const repo = 'https://robust-lobsters.github.io/robust-lobsters.github.io'
 
 const withVanillaExtract = createVanillaExtractPlugin()
 const nextConfig: NextConfig = {
-  basePath: process.env.NODE_ENV === 'production' ? `/${repo}` : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? `/${repo}/` : '',
+  basePath: process.env.NODE_ENV === 'production' ? `${repo}` : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? `${repo}/` : '',
   output: 'export',
   distDir: 'out',
 }
